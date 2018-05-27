98102242cd54f8fc3317008d378564fdcc5f25f0
Chaitanya Asawa <casawa@stanford.edu>
2018-05-26 20:36:39 -0700

    Creating a GUI for executing arbitrary commands

--- jsk_rqt_plugins/src/jsk_rqt_plugins/button.py
+++ jsk_rqt_plugins/src/jsk_rqt_plugins/button.py
@@ -1,5 +1,6 @@
 from rqt_gui_py.plugin import Plugin
 from jsk_rqt_plugins.button_general import ServiceButtonGeneralWidget
+from jsk_rqt_plugins.command_button import CommandButtonGeneralWidget
 
 class ServiceButtons(Plugin):
     """
@@ -23,3 +24,26 @@ class ServiceButtonWidget(ServiceButtonGeneralWidget):
     """
     def __init__(self):
         super(ServiceButtonWidget, self).__init__(button_type = "push")
+
+class CommandButtons(Plugin):
+    """
+    rqt class to provide multiple buttons
+    """
+    def __init__(self, context):
+        super(CommandButtons, self).__init__(context)
+        self.setObjectName("CommandButtons")
+        self._widget = CommandButtonWidget()
+        context.add_widget(self._widget)
+    def save_settings(self, plugin_settings, instance_settings):
+        self._widget.save_settings(plugin_settings, instance_settings)
+    def restore_settings(self, plugin_settings, instance_settings):
+        self._widget.restore_settings(plugin_settings, instance_settings)
+    def trigger_configuration(self):
+        self._widget.trigger_configuration()
+
+class CommandButtonWidget(CommandButtonGeneralWidget):
+    """
+    Qt widget to visualize multiple buttons
+    """
+    def __init__(self):
+        super(CommandButtonWidget, self).__init__(button_type = "push")
