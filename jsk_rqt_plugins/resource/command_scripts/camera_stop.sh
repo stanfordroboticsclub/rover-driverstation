fbb8f3c4d5d3effc9f544c9cdf1d506674bbd66f
Stanford Robotics <stanford@robotics.com>
2018-05-28 22:12:22 -0700

    Fix brief camera bugs

--- jsk_rqt_plugins/resource/command_scripts/camera_stop.sh
+++ jsk_rqt_plugins/resource/command_scripts/camera_stop.sh
@@ -1,2 +1,2 @@
-gnome-terminal -x bash -c 'ssh nvidia@192.168.1.10 /home/nvidia/catkin_ws/src/rover_vsiion/scripts/launchCameras.sh stop $1' &
-gnome-terminal -x bash -c 'source /home/rover/catkin_ws/src/rover-driverstation/jsk_rqt_plugins/resource/command_scripts/cameraListener.sh stop $1' &
+gnome-terminal -x bash -c "echo ./launchCameras.sh; ssh nvidia@192.168.1.10 /home/nvidia/catkin_ws/src/rover_vision/scripts/launchCameras.sh stop $1; exec bash"
+gnome-terminal -x bash -c "echo ./cameraListener.sh; source /home/rover/catkin_ws/src/rover-driverstation/jsk_rqt_plugins/resource/command_scripts/cameraListener.sh stop $1; exec bash"
